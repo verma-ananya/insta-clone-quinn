daff55b680be7c52b44d8b9c8cd1a77601a69354
Ananya Verma <lincolnav829@gmail.com>
2020-10-05 18:13:01 +0530

    Updates

---
+++ precache-manifest.afd10a63742b304139fe5e5d3a44e353.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb865509517f1583acdcb2961ca7ad0f",
+    "url": "/insta-clone-quinn/index.html"
+  },
+  {
+    "revision": "6f2ca42b46b6fa62471e",
+    "url": "/insta-clone-quinn/static/css/2.d0eaa8fd.chunk.css"
+  },
+  {
+    "revision": "67d6857002c6b729d17d",
+    "url": "/insta-clone-quinn/static/css/main.53f700d1.chunk.css"
+  },
+  {
+    "revision": "6f2ca42b46b6fa62471e",
+    "url": "/insta-clone-quinn/static/js/2.0efa720d.chunk.js"
+  },
+  {
+    "revision": "f08e720869e7a847549fb58c8267edec",
+    "url": "/insta-clone-quinn/static/js/2.0efa720d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67d6857002c6b729d17d",
+    "url": "/insta-clone-quinn/static/js/main.87f722fa.chunk.js"
+  },
+  {
+    "revision": "974432c5dcd415402882",
+    "url": "/insta-clone-quinn/static/js/runtime-main.b98c3afe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/insta-clone-quinn/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f02fbd329eb5b8e80e15d9fb4cbb959f",
+    "url": "/insta-clone-quinn/static/media/sofia-regular.f02fbd32.otf"
+  }
+]);
